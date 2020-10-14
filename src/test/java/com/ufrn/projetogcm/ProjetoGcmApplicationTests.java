0c60f3c217c4da79bb00c0448cb48c7abe50408a
Amanda Priscilla <amandapriscilla@users.noreply.github.com>
2020-10-14 01:07:05 -0300

    Projeto inicial do Spring Boot

---
+++ src/test/java/com/ufrn/projetogcm/ProjetoGcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufrn.projetogcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
