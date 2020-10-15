94c43f061aa02ab0d76980c5a77510041c5faf2d
Amanda Priscilla <amandapriscilla@users.noreply.github.com>
2020-10-14 22:58:02 -0300

    Adicionando suporte a testes unitarios e iniciando casos de testes

--- src/test/java/com/ufrn/gcm/ProjetoGcmApplicationTests.java
+++ src/test/java/com/ufrn/gcm/ProjetoGcmApplicationTests.java
@@ -1,13 +1,48 @@
 package com.ufrn.gcm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.math.BigDecimal;
+
+import com.ufrn.gcm.service.BancoService;
 
 @SpringBootTest
 class ProjetoGcmApplicationTests {
 	
+	@Autowired
+	private BancoService bancoService;
+	
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertNotNull(bancoService);
 	}
-
+	
+	@Test
+	void testarVerSaldo() throws Exception {
+		assertEquals(bancoService.verSaldo(12345), BigDecimal.ZERO);
+	}
+	
+	@Test
+	void testarCreditar() throws Exception {
+		assertEquals(bancoService.creditarConta(12345, BigDecimal.TEN), BigDecimal.TEN);
+	}
+	
+	@Test
+	void testarDebitarSaldoInsuficiente() throws Exception {
+		assertThrows(Exception.class, () -> {
+			bancoService.debitarConta(12345, new BigDecimal(200));
+		});
+	}
+	
+	@Test
+	void testarTransferirSaldoInsuficiente() throws Exception {
+		assertThrows(Exception.class, () -> {
+			bancoService.transferir(12345, 23456, new BigDecimal(200));
+		});
+	}
 }
